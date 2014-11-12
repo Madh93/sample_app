d98bebf20e25b4d6d0510c30d9732429147f6f5e
Madh93 <michelangelo93@gmail.com>
2014-11-12 22:46:53 +0000

    Redirigir post-registro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,12 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    if @user.save
-      redirect_to @user
-    else
-      render 'new'
-    end    
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
