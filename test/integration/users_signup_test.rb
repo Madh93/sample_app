2358040e7f96add526171634ec939372734fa248
Madh93 <michelangelo93@gmail.com>
2014-11-12 23:00:19 +0000

    Tests para un registro con exito

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,7 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
     get signup_path
-    assert_no_difference 'User.count' do
+    assert_no_difference 'User.count' do        #El nº de usuarios no se ve alterado
       post users_path, user: { name:  "",
                                email: "user@invalid",
                                password:              "foo",
@@ -12,4 +12,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do        #El nº de usuarios deberia aumentar en 1
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+  end  
 end
